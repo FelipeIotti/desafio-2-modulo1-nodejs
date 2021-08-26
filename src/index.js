44fdfe73c917f872859dd85895a9deb08f72dc4e
FelipeIotti <felipeizago@hotmail.com>
2021-08-26 15:25:59 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,80 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((user)=> user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error: 'Mensagem do erro'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} =request; 
+
+  if(!user) {
+    return response.status(404).json({error: 'Mensagem do erro'});
+  }
+
+  if (user.pro === true){
+    return next();
+  }
+  else if (user.pro === true){
+    return response.status(403).json({error: 'Mensagem do erro'});
+  }
+
+  else if (user.pro === false & user.todos.length <10){
+    return next();
+  }
+
+  else {
+    return response.status(403).json({error: 'Mensagem do erro'});
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username} = request.headers;
+  const {id} = request.params;
+
+  const user = users.find((user)=> user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error: 'Mensagem do erro'});
+  }
+
+  if(validate(id)===false) {
+    return response.status(400).json({error: 'Mensagem do erro'});
+  }
+
+  const todo = user.todos.find(todo=> todo.id === id); 
+
+  if(!todo) {
+    return response.status(404).json({error: 'Mensagem do erro'});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find((user)=> user.id === id);
+
+  if(!user) {
+    return response.status(404).json({error: 'Mensagem do erro'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
